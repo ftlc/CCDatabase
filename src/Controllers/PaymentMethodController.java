dcdec835f64da0ed53f886983df170e3f12906dc
ftlc <harrysadoyan@gmail.com>
2016-12-21 22:04:08 -0500

    InfoPageFillOut

---
+++ src/Controllers/PaymentMethodController.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by ftlc on 12/21/16.
+ */
+public class PaymentMethodController implements ActionListener{
+    public PaymentMethodController(){}
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/Controllers/SoldFromController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by ftlc on 12/21/16.
+ */
+public class SoldFromController implements ActionListener{
+
+    public SoldFromController(){}
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
